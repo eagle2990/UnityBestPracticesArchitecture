9b1b94de034ebd809e94875f89f553551b8f968c
eagle2990 <eagle2990@gmail.com>
2017-12-19 20:16:33 -0500

    Updating Project to run in command line and adding travis scripts

--- Assets/Engine/ScriptableObjectUtility.cs
+++ Assets/Engine/ScriptableObjectUtility.cs
@@ -1,4 +1,3 @@
-using UnityEditor;
 using UnityEngine;
 
 namespace RealDreams.Studio.Engine
@@ -14,7 +13,7 @@ namespace RealDreams.Studio.Engine
 		public static void CreateAsset<T>() where T : ScriptableObject
 		{
 			var asset = ScriptableObject.CreateInstance<T>();
-			ProjectWindowUtil.CreateAsset(asset, "New " + typeof(T).Name + ".asset");
+			//ProjectWindowUtil.CreateAsset(asset, "New " + typeof(T).Name + ".asset");
 		}
 	}
 }
---
+++ CI/install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Installing Unity3D Editor in Linux using Env Variables defined in .travis.yml file.
+# Current link is for 2017.2.0.f3, which is a beta for Unity3D Linux. The full information of
+# latests Linux versions can be found in
+# https://forum.unity.com/threads/unity-on-linux-release-notes-and-known-issues.350256/page-2
+
+check_file_exists_or_download() {
+    if [ -e $1 ]
+    then
+        echo "File $1 exists so it's not neccesary to download"
+    else
+        wget -O $1 $2
+    fi
+}
+
+UNITY_INSTALLER="Unity.sh"
+echo 'Downloading Unity Installer'
+if [ "$TRAVIS_OS_NAME" = "linux" ]
+then
+    UNITY_INSTALLER="Unity.sh"
+    check_file_exists_or_download $UNITY_INSTALLER $UNITY_LINUX_DOWNLOAD
+    echo 'Installing $UNITY_INSTALLER for Linux'
+    sudo chmod +x $UNITY_INSTALLER
+    sudo echo | sudo ./$UNITY_INSTALLER -o $UNITY_ROOT_PATH
+else
+    UNITY_INSTALLER="Unity.pkg"
+    check_file_exists_or_download $UNITY_INSTALLER $UNITY_MACOS_DOWNLOAD
+    echo 'Installing $UNITY_INSTALLER for MacOS'
+    sudo installer -dumplog -package $UNITY_INSTALLER -target $UNITY_ROOT_PATH
+fi
