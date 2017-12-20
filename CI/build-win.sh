b34ab5da7e89806f76d4523efabea03da301a47d
eagle2990 <eagle2990@gmail.com>
2017-12-19 20:45:53 -0500

    Updating scripts for CI

---
+++ CI/build-win.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Command-line to build project finding the Unity3d Installation Path, saving it in a variable
+# and building for Windows
+
+UNITY_INSTALLATION=$(find / -mindepth 1 -maxdepth 1 -type d -name "unity*")
+
+echo "Attempting to build $UNITY_PROJECT"
+$UNITY_INSTALLATION/Editor/Unity \
+    -batchmode \
+    -nographics \
+    -silent-crashes \
+    -logFile "$(pwd)/unity.log" \
+    -projectPath "$(pwd)" \
+    -buildWindowsPlayer "$(pwd)/Build/$UNITY_PROJECT.exe" \
+    -quit
---
+++ CI/editor-tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script to run Editor Tests, finding the installation path of Unity3D
+
+UNITY_INSTALLATION=$(find / -mindepth 1 -maxdepth 1 -type d -name "unity*")
+
+echo "Running tests of $UNITY_PROJECT"
+$UNITY_INSTALLATION/Editor/Unity \
+    -runTests \
+    -logFile "$(pwd)/unity-editor-tests.log" \
+    -projectPath "$(pwd)" \
+    -testResults "$(pwd)/editor-tests-results.xml" \
+    -testPlatform editmode \
+    -quit
