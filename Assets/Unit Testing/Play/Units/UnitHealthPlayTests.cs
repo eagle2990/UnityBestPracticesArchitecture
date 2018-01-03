9c3707f633af701e220d012c3781a539f7588426
eagle2990 <eagle2990@gmail.com>
2018-01-03 13:05:07 -0500

    Removing unnecessary comments and variables.

--- Assets/Unit Testing/Play/Units/UnitHealthPlayTests.cs
+++ Assets/Unit Testing/Play/Units/UnitHealthPlayTests.cs
@@ -11,37 +11,43 @@ public class UnitHealthPlayTests {
 	GameObject EnemyPrefab;
 
 	GameObject bot;
-	UnitData botStats;
-	FloatVariable botHP;
-
 	GameObject enemy;
-	EnemyData enemyStats;
 
 	[OneTimeSetUp]
 	public void Init()
 	{
 		PlayerPrefab = Resources.Load<GameObject>("Units/Player/Player");
+		bot = CreateGameObject(PlayerPrefab);
+	}
+
+	void CreateEnemy()
+	{
 		EnemyPrefab = Resources.Load<GameObject>("Units/Enemies/DumbEnemy/DumbEnemy");
+		enemy = CreateGameObject(EnemyPrefab);
+	}
 
-		//botHP = ScriptableObject.CreateInstance<FloatVariable>();
-
-		//botStats = ScriptableObject.CreateInstance<PlayerData>();
-		//botStats.MaxHP = new FloatReference(120f);
-		//botStats.HP = botHP;
-
-		bot = CreateGameObject(PlayerPrefab, botStats);
+	void CreatePlane()
+	{
+		GameObject.CreatePrimitive(PrimitiveType.Plane);
 	}
 
 	[UnityTest]
 	public IEnumerator UnitHealtTest_StartsWithMaxHP() {
 		yield return null;
-		botStats = bot.GetComponent<UnitHealth>().BaseStats;
+		UnitData botStats = bot.GetComponent<UnitHealth>().BaseStats;
 		Assert.AreEqual(botStats.MaxHP.Value, botStats.HP.Value);
 	}
 
-	[Test]
-	public void UnitHealtTest_RecievesDamage()
+	[UnityTest]
+	public IEnumerator UnitHealtTest_RecievesDamage()
 	{
+		//yield return null;
+		//CreatePlane();
+		//CreateEnemy();
+
+		//bot.transform.position = enemy.transform.position;
+		//UnitData enemyStats = enemy.GetComponent<UnitHealth>().BaseStats;
+		//Assert.AreNotEqual(enemyStats.MaxHP.Value, enemyStats.HP.Value);
 		throw new NotImplementedException();
 	}
 
@@ -57,11 +63,9 @@ public class UnitHealthPlayTests {
 		throw new NotImplementedException();
 	}
 
-	private GameObject CreateGameObject(GameObject prefab, UnitData stats)
+	private GameObject CreateGameObject(GameObject prefab)
 	{
 		GameObject character = GameObject.Instantiate(prefab);
-		//UnitHealth health = character.GetComponent<UnitHealth>();
-		//health.BaseStats = stats;
 		return character;
 	}
 }
